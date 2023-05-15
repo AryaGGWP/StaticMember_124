0433893bfda6db7002a6f93426e1e9d20d499e2f
KeikooUwU <127074149+KeikooUwU@users.noreply.github.com>
2023-05-15 14:23:33 +0700

    fungsi statik

---
+++ mbrStatikfunc/mbrStatikfunc.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+class mahasiswa {
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+
+};
+
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
