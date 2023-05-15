5fcdda8c3a7d9b616e194a90c7d385c61300ca8a
KeikooUwU <127074149+KeikooUwU@users.noreply.github.com>
2023-05-15 11:28:31 +0700

    1st commit

--- StaticMember_124/StaticMember_124.cpp
+++ StaticMember_124/StaticMember_124.cpp
@@ -2,7 +2,16 @@
 #include <string>
 using namespace std:
 
+class mahasiswa {
+public:
+    static int nim:
+    int id:
+    string nama;
 
+    void setID();
+    void printA11();
+    mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
 
 int main()
 {
